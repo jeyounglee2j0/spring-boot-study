e49b678fd1d5ca7b64743af22a687569ad03a982
jeyoung_lee <whatareyou1005@gmail.com>
2026-09-01 21:43:13 +0900

    9.1 게시글 생성 및 조회 기능 구현

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,11 +3,17 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired
@@ -19,13 +25,31 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
-        //dto를 엔티티로 변환
+        log.info(form.toString());
         Article article = form.toEntity();
-        System.out.println(article.toString());
-        //라파지터리로 엔티티를 db저장
+        log.info(article.toString());
         Article saved = articleRepository.save(article);
-        System.out.println(article.toString());
-        return"articles/new";
+        log.info(saved.toString());
+        return"redirect:/articles/" + saved.getId();
+    }
+    @GetMapping ("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id = " + id);
+        //id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //모델에 데이터 등록하기
+        model.addAttribute("articles", articleEntity);
+        //뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        //모든 데이터 가져오기
+        ArrayList<Article> articleList = articleRepository.findAll();
+        //모델에 데이터 등록하기
+        model.addAttribute("articleList", articleList);
+        //뷰 페이지 설정하기
+        return "articles/index";
     }
 }
---
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -0,0 +1,52 @@
+package com.example.firstproject.controller;
+
+
+import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Member;
+import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+
+
+@Slf4j
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @GetMapping("/signup")
+    public String sginUpPage(){
+        return "members/new";
+    }
+
+    @PostMapping("/join")
+    public String createMember(MemberForm form){
+        log.info(form.toString());
+        Member member = form.toEntity();
+        log.info(member.toString());
+        Member saved = memberRepository.save(member);
+        log.info(saved.toString());
+        return "/signup";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        ArrayList<Member> memberList = memberRepository.findAll();
+        model.addAttribute("memberList", memberList);
+        return "members/index";
+    }
+}
