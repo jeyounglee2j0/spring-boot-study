4676697ca74496f4755817c2a1af576772b134bd
jeyoung_2 <jeyounglee.2j0@gmail.com>
2026-09-17 21:25:25 +0900

    service랑 transaction 전까지

---
+++ src/main/java/com/example/firstproject/api/CoffeeApiController.java
@@ -0,0 +1,57 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.dto.CoffeeDto;
+import com.example.firstproject.entity.Coffee;
+import com.example.firstproject.repository.CoffeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Slf4j
+@RestController
+public class CoffeeApiController {
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+    //GET
+    @GetMapping("/api/coffees")
+    public List<Coffee> index(){
+        return coffeeRepository.findAll();
+    }
+    @GetMapping("/api/coffees/{id}")
+    public ResponseEntity<Coffee> show(@PathVariable Long id){
+        Coffee coffee = coffeeRepository.findById(id).orElse(null);
+        return (coffee != null)?
+                ResponseEntity.status(HttpStatus.OK).body(coffee):
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    }
+    //POST
+    @PostMapping("/api/coffes")
+    public ResponseEntity<Coffee> create(@RequestBody CoffeeDto coffeeDto){
+        Coffee coffee = coffeeDto.toEntity();
+        if(coffee.getId() != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+
+        }
+        Coffee created = coffeeRepository.save(coffee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+    //PATCH
+    @PatchMapping("/api/coffees/{id}")
+    public ResponseEntity<Coffee> update(@PathVariable Long id, @RequestBody CoffeeDto coffeeDto){
+        Coffee coffee = coffeeDto.toEntity();
+        log.info("id:{}, coffee:{}", id, coffee.toString());
+
+        Coffee target = coffeeRepository.findById(id).orElse(null);
+        if(target == null || !id.equals(coffee.getId())){
+            log.info("잘못된 요청! id:{}, coffee:{}", id, coffee.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        target.patch(coffee);
+        Coffee updated = coffeeRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+}
